4ae44c89e8d5f06b2024d0a6e767a87ed4c8e615
gilian <honkoopgilian@gmail.com>
2025-01-31 21:19:03 +0100

    added additional comments and abstracted code

--- GNNBoundary/lib/helpers.py
+++ GNNBoundary/lib/helpers.py
@@ -4,7 +4,7 @@ import os
 import random
 import seaborn as sns
 import matplotlib.pyplot as plt
-# sns.set_style("white")
+import json
 sns.set(font_scale=1.6)
 
 def set_seed(seed):
@@ -51,3 +51,44 @@ def draw_matrix_colorless(matrix, names, fmt='.2f', vmin=0, vmax=None,
     if ylabel:
         plt.ylabel(ylabel)
     plt.show()
+
+def save_json(dataset_name, data):
+    formatted_data = format_results(data)
+
+    with open(f"logs/{dataset_name}.json", 'w', encoding='utf-8') as f: 
+        json.dump(formatted_data, f, ensure_ascii=False, indent=4)
+
+def load_json(dataset_name):
+    with open(f"logs/{dataset_name}.json", 'r') as f:
+        loaded = json.load(f)
+    
+    results = format_loaded(loaded)
+
+    return results
+
+def format_results(results):
+    results_formatted = dict()
+    for k, v in results.items():
+        results_formatted[f"{k[0]},{k[1]}"] = v.tolist()
+
+    return results_formatted
+
+def format_loaded(loaded):
+    results = dict()
+    for k,v in loaded.items():
+        results[(k[0],k[2])] = np.array(v)
+    
+    return results
+
+def print_results(results):
+    for k, v in results.items():
+        print(f"c1 = {k[0]} c2 = {k[1]}")
+        print(f'gnnboundary: c1_mean = {v[0]} c1_std = {v[1]} c2_mean = {v[2]} c2_std = {v[3]}')
+        print(f'baseline: c1_mean = {v[4]} c1_std = {v[5]} c2_mean = {v[6]} c2_std = {v[7]}')
+
+def print_results_3way(results_3way):
+    for k, v in results_3way.items():
+        print(f"c1 = {k[0]} c2 = {k[1]} c3 = {k[2]}")
+        print(f'gnnboundary: c1_mean = {v[0]} c1_std = {v[1]} c2_mean = {v[2]} c2_std = {v[3]} c3_mean = {v[4]} c3_std = {v[5]}')
+        print(f'baseline: c1_mean = {v[6]} c1_std = {v[7]} c2_mean = {v[8]} c2_std = {v[9]} c3_mean = {v[10]} c3_std = {v[11]}')
+        print(f"convergence rate = {v[12]}")
